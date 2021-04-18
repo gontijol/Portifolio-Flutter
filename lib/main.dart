f892925a9e94c7d4bbdfec0710a3e8d59a7877a4
Luigi Gontijo <gontijol@discente.ufg.br>
2021-04-18 19:49:04 -0300

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Luigi Gontijo',
       theme: ThemeData(
         inputDecorationTheme: kDefaultInputDecorationTheme,
